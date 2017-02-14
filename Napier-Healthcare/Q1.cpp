2b03bd77a06e0b343174bdd3f0cea688844dbd59
cfx.404 <C-F-4@users.noreply.github.com>
2017-02-14 23:12:36 +0530

    Update Q1.cpp

--- Napier-Healthcare/Q1.cpp
+++ Napier-Healthcare/Q1.cpp
@@ -5,17 +5,24 @@
  */
  
 #include <iostream>
-#include <sstreams>
+#include <sstream>
 
-using namespace std;
-
-string str_encode(string str) {
-  stringstream ss;
-  return ss.tostr();
+std::string StrEncode(std::string str_to_encode) {
+  std::stringstream ss;
+  for(int i = 0; str_to_encode[i]; i++) {
+    int ct = 0;
+    ss << str_to_encode[i];
+    while(str_to_encode[i] == str_to_encode[i+1]) {
+      i++;
+      ct++;
+    }
+    ss << ct;
+  }
+  return ss.str();
 }
 
 int main(void) {
-  string str;
+  std::string str;
   cin >> str;
   cout << str_encode(str) << endl;
 }
